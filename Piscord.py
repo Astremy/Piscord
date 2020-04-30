5fdda8febe55752d31447d40c1d352b52d59cdb9
Astremy <43485551+Astremy@users.noreply.github.com>
2020-04-30 13:28:32 -0300

    Create Piscord.py

---
+++ Piscord.py
@@ -0,0 +1,171 @@
+import asyncio
+import json
+import aiohttp
+from threading import Thread
+import time
+
+class Bot(Thread):
+
+	def __init__(self,token):
+		super().__init__()
+		self.utility = Utility(self)
+		self.token=token
+		self.url="https://discordapp.com/api"
+		self.__last_sequence = ""
+
+	async def api_call(self,path, method="GET", **kwargs):
+		defaults = {
+			"headers": {
+				"Authorization": f"Bot {self.token}",
+				"User-Agent": "Test Bot"
+			}
+		}
+		kwargs = dict(defaults, **kwargs)
+		async with aiohttp.ClientSession() as session:
+			async with session.request(method, self.url+path,**kwargs) as response:
+				try:
+					assert 200 == response.status, response.reason
+					return await response.json()
+				except:return...
+
+	async def begin(self):
+		response = await self.api_call("/gateway")
+		await self.__main(response["url"])
+
+	async def __main(self,url):
+		async with aiohttp.ClientSession() as session:
+			async with session.ws_connect(f"{url}?v=6&encoding=json") as ws:
+				async for msg in ws:
+					data = json.loads(msg.data)
+
+					if data["op"] == 10:
+						asyncio.create_task(self.__heartbeat(ws,data["d"]["heartbeat_interval"]))
+						await ws.send_json({
+							"op": 2,
+							"d": {
+								"token": self.token,
+								"properties": {},
+								"compress": False,
+								"large_threshold": 250
+						}})
+					elif data["op"] == 11:
+						pass
+					elif data["op"] == 0:
+						if data["t"]=="MESSAGE_CREATE":
+							if data["d"]["content"]=="Test":
+								await self.api_call(f"/channels/{data['d']['channel_id']}/messages","POST", json={"content": "Hello !"})
+							else:
+								print(data["t"],data["d"])
+						if data["t"] == "MESSAGE_REACTION_ADD":
+							print(data["t"],data["d"])
+						self.__last_sequence=data["s"]
+
+	async def __heartbeat(self,ws, interval):
+		while True:
+			await asyncio.sleep(interval / 1000)
+			await ws.send_json({"op": 1,"d": self.__last_sequence})
+
+	def run(self):
+		self.loop = asyncio.new_event_loop()
+		try:
+			self.loop.run_until_complete(self.begin())
+		except RuntimeError:print("Bot éteint")
+
+	def stop(self):
+		try:
+			self.loop.stop()
+		except:...
+		try:
+			for x in asyncio.all_tasks():
+				try:
+					x.cancel()
+				except:...
+		except:...
+
+class Utility:
+
+	def __init__(self,bot):
+		self.bot = bot
+
+	def send_message(self,channel,content):
+		return Message(asyncio.run(self.bot.api_call(f"/channels/{channel}/messages", "POST", json={"content": content})),self.bot)
+
+	def get_guild(self,guild_id):
+		return Guild(asyncio.run(self.bot.api_call(f"/guilds/{guild_id}","GET")),self.bot)
+
+	def get_channel(self,channel_id):
+		return Channel(asyncio.run(self.bot.api_call(f"/channels/{channel_id}","GET")),self.bot)
+
+class Guild:
+
+	def __init__(self, guild, bot):
+		self.id = guild["id"]
+		self.name = guild["name"]
+		self.description = guild["description"]
+		self.emojis = [Emoji(emoji) for emoji in guild["emojis"]]
+		self.__bot = bot
+
+	def get_channels(self):
+		channels = asyncio.run(self.__bot.api_call(f"/guilds/{self.id}/channels"))
+		return [Channel(channel,self.__bot) for channel in channels]
+
+class Channel:
+
+	def __init__(self,channel,bot):
+		self.id = channel["id"]
+		self.type = channel["type"]
+		self.name = channel["name"]
+		self._guild = channel["guild_id"]
+		self.__bot = bot
+
+	def edit(self,modifs):
+		asyncio.run(self.__bot.api_call(f"/channels/{self.id}","PATCH",json=modifs))
+
+class Message:
+
+	def __init__(self, message, bot):
+		self.id = message["id"]
+		self.content = message["content"]
+		self.channel = message["channel_id"]
+		self.author = User(message["author"])
+		self.__bot = bot
+
+	def delete(self):
+		asyncio.run(self.__bot.api_call(f"/channels/{self.channel}/messages/{self.id}","DELETE"))
+
+	def edit(self,content):
+		asyncio.run(self.__bot.api_call(f"/channels/{self.channel}/messages/{self.id}","PATCH",json={"content": content}))
+
+	def add_reaction(self, reaction):
+		asyncio.run(self.__bot.api_call(f"/channels/{self.channel}/messages/{self.id}/reactions/{reaction}/@me","PUT"))
+
+	def delete_reactions(self):
+		asyncio.run(self.__bot.api_call(f"/channels/{self.channel}/messages/{self.id}/reactions","DELETE"))
+
+
+class User:
+
+	def __init__(self,user):
+		self.id = user["id"]
+		self.name = user["username"]
+		self.discriminator = user["discriminator"]
+		self.bot = user["bot"]
+		self.avatar = f"https://cdn.discordapp.com/avatars/{self.id}/{user['avatar']}.png"
+
+class Reaction:
+
+	def __init__(self,reaction,bot):
+		self.user = User(reaction["member"]["user"])
+		self.channel = reaction["channel_id"]
+		self.guild = reaction["guild_id"]
+		self._message = reaction["message_id"]
+		self.__bot = bot
+
+	def get_message(self):
+		return Message(self.__bot.api_call(f"/channels/{self.channel}/messages/{self._message}"),self.__bot)
+
+class Emoji:
+
+	def __init__(self,emoji):
+		self.name = emoji["name"]
+		self.id = emoji["id"]
