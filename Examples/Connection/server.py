8a488dd00e116f0c5427581969d6d2d0ac61394b
Astremy <43485551+Astremy@users.noreply.github.com>
2020-04-30 23:58:05 -0300

    Update server.py

--- Examples/Connection/server.py
+++ Examples/Connection/server.py
@@ -4,7 +4,7 @@ import os
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
 
-bot = Bot("NzA1NTE1ODMzNjk0MzU1NTY2.XqtrrA.FBQr0JNbjHPic6IksPSIh88FJH4")
+bot = Bot("Bot Token")
 
 @bot.event("on_ready")
 def on_ready(user):
@@ -23,30 +23,7 @@ bot.start()
 
 site = Server("127.0.0.1",80)
 
-auth = OAuth(bot,"fT9dVgqu2DU72nr0bpdkZx2pZIQZ34AG","http://127.0.0.1/connect","identify guilds")
-
-"""
-@site.path("/")
-def index(user):
-	z=[]
-	for guild in bot.get_self_guilds():
-		z.append(f"<p>{guild.name} :</p>")
-		for channel in guild.get_channels():
-			z.append(f"<p>{channel.name} : {channel.id}<p>")
-	return template("index.html",chann="".join(z))
-
-@site.path("/send_message")
-@methods("POST")
-def add_reaction(user):
-	if "message" in user.request.form:
-		if "channel" in user.request.form:
-			message = user.request.form["message"].replace("+"," ")
-			clean_message = message.split("%")
-			for i in range(1,len(clean_message)):
-				clean_message[i] = chr(int(clean_message[i][:2],16))+clean_message[i][2:]
-			bot.send_message(user.request.form["channel"],"".join(clean_message))
-	return redirect(user,"/")
-"""
+auth = OAuth(bot,"Bot Secret","http://127.0.0.1/connect","identify guilds")
 
 @site.path("/")
 def index(user):
