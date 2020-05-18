5a35ef346f7de250f29761d24ecac2c11ed34864
Astremy <43485551+Astremy@users.noreply.github.com>
2020-05-18 15:28:33 -0300

    :arrow_up: update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Piscord",
-    version="1.2.0",
+    version="1.3.0",
     author="Astremy",
     description="Piscord is a python framework to communicate with the Discord api.",
     long_description=long_description,
