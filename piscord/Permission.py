dc57b38f4927d19ebf9763d91576652f58ebb1c5
shika-blyat <abdelzighem1@gmail.com>
2020-06-08 16:44:54 +0200

    BREAKING CHANGE move builtins permissions into an enum

--- piscord/Permission.py
+++ piscord/Permission.py
@@ -1,42 +1,38 @@
+from enum import IntEnum
+
+
 class Perm:
+    CREATE_INSTANT_INVITE = 0
+    KICK_MEMBERS = 2**1
+    BAN_MEMBERS = 2**2
+    ADMINISTRATOR = 2**3
+    MANAGE_CHANNELS = 2**4
+    MANAGE_GUILD = 2**5
+    ADD_REACTIONS = 2**6
+    VIEW_AUDIT_LOG = 2**7
+    PRIORITY_SPEAKER = 2**8
+    STREAM = 2**9
+    VIEW_CHANNEL = 2**10
+    SEND_MESSAGES = 2**11
+    SEND_TTS_MESSAGES = 2**12
+    MANAGE_MESSAGES = 2**13
+    EMBED_LINKS = 2**14
+    ATTACH_FILES = 2**15
+    READ_MESSAGE_HISTORY = 2**16
+    MENTION_EVERYONE = 2**17
+    USE_EXTERNAL_EMOJIS = 2**18
+    VIEW_GUILD_INSIGHTS = 2**19
+    CONNECT = 2**20
+    SPEAK = 2**21
+    MUTE_MEMBERS = 2**22
+    DEAFEN_MEMBERS = 2**23
+    MOVE_MEMBERS = 2**24
+    USE_VAD = 2**25
+    CHANGE_NICKNAME = 2**26
+    MANAGE_NICKNAMES = 2**27
+    MANAGE_ROLES = 2**28
+    MANAGE_WEBHOOKS = 2**29
+    MANAGE_EMOJIS = 2**30
 
-	def __init__(self,number):
-		self.number = 2**number
-
-	def __eq__(self,n):
-		if n & self.number == self.number:
-			return True
-		else:
-			return False
-
-create_instant_invite = Perm(0)
-kick_members = Perm(1)
-ban_members = Perm(2)
-administrator = Perm(3)
-manage_channels = Perm(4)
-manage_guild = Perm(5)
-add_reactions = Perm(6)
-view_audit_log = Perm(7)
-priority_speaker = Perm(8)
-stream = Perm(9)
-view_channel = Perm(10)
-send_messages = Perm(11)
-send_tts_messages = Perm(12)
-manage_messages = Perm(13)
-embed_links = Perm(14)
-attach_files = Perm(15)
-read_message_history = Perm(16)
-mention_everyone = Perm(17)
-use_external_emojis = Perm(18)
-view_guild_insights = Perm(19)
-connect = Perm(20)
-speak = Perm(21)
-mute_members = Perm(22)
-deafen_members = Perm(23)
-move_members = Perm(24)
-use_vad = Perm(25)
-change_nickname = Perm(26)
-manage_nicknames = Perm(27)
-manage_roles = Perm(28)
-manage_webhooks = Perm(29)
-manage_emojis = Perm(30)
+    def __eq__(self, other):
+        return other & self == self
